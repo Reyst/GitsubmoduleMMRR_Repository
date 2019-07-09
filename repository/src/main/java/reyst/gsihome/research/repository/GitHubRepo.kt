552bd7e245b1c6b8ff792086a57b987748b38ddb
Reyst <oleksenko.pm@gmail.com>
2019-07-09 19:22:57 +0300

    Revert "1st part of changes"
    This reverts commit cb75e3fe

--- repository/src/main/java/reyst/gsihome/research/repository/GitHubRepo.kt
+++ repository/src/main/java/reyst/gsihome/research/repository/GitHubRepo.kt
@@ -6,9 +6,8 @@ import reyst.gsihome.research.repository.core.IRepoDto
 data class GitHubRepo(
     override val id: Int,
     override val name: String,
-    override val fullName: String,
-    override val isChecked: Boolean
+    override val fullName: String
 ): Repo
 
-fun IRepoDto.toDomain() = GitHubRepo(id, name, fullName, isChecked)
+fun IRepoDto.toDomain() = GitHubRepo(id, name, fullName)
 fun Collection<IRepoDto>.toDomain() = map { it.toDomain() }
