cb75e3fe395676fa20680fc236bec96c349704de
Reyst <oleksenko.pm@gmail.com>
2019-07-09 19:21:31 +0300

    1st part of changes

--- repository/src/main/java/reyst/gsihome/research/repository/GitHubRepo.kt
+++ repository/src/main/java/reyst/gsihome/research/repository/GitHubRepo.kt
@@ -6,8 +6,9 @@ import reyst.gsihome.research.repository.core.IRepoDto
 data class GitHubRepo(
     override val id: Int,
     override val name: String,
-    override val fullName: String
+    override val fullName: String,
+    override val isChecked: Boolean
 ): Repo
 
-fun IRepoDto.toDomain() = GitHubRepo(id, name, fullName)
+fun IRepoDto.toDomain() = GitHubRepo(id, name, fullName, isChecked)
 fun Collection<IRepoDto>.toDomain() = map { it.toDomain() }
